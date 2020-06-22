c273428f651df19573d070d20e34c2617f42a14d
whatYnext <63004699+whatYnext@users.noreply.github.com>
2020-06-22 12:55:42 +0200

    Add files via upload

---
+++ FCN/FCN.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of Full Convolutional Network 
+
+1. Matrix manipulation to complement convolution computation
+
+2. Using transpose convolution layer
+
+3. Transpose layer can be initialized by bilinear kernel
+
+@author: mayao
+"""
+
+import d2lzh as d2l
+from mxnet import gluon, image, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, model_zoo, nn
+import numpy as np
+import sys
+import os
+
+# Model set up
+pretrained_net = model_zoo.vision.resnet18_v2(pretrained=True)
+net = nn.HybridSequential()
+for layer in pretrained_net.features[:-2]:
+    net.add(layer)
+    
+num_classes = 21
+net.add(nn.Conv2D(num_classes, kernel_size=1),
+        nn.Conv2DTranspose(num_classes, kernel_size=64, padding=16,
+                           strides=32))
+
+#Bilinear kernel with tranpose convolution is the bilinear interpolation 
+def bilinear_kernel(in_channels, out_channels, kernel_size):
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:kernel_size, :kernel_size]
+    filt = (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
+    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
+                      dtype='float32')
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return nd.array(weight)
+
+net[-1].initialize(init.Constant(bilinear_kernel(num_classes, num_classes,
+                                                 64)))
+
+#net[-1].initialize(init=init.Xavier())
+
+net[-2].initialize(init=init.Xavier())
+
+
+# Read train and test data
+crop_size, batch_size, colormap2label = (320, 480), 4, nd.zeros(256**3)
+for i, cm in enumerate(d2l.VOC_COLORMAP):
+    colormap2label[(cm[0] * 256 + cm[1]) * 256 + cm[2]] = i
+#voc_dir = d2l.download_voc_pascal(data_dir='C:/Users/mayao/Desktop/Python learning/MXnet/data')
+
+data_dir='C:/Users/mayao/Desktop/Python learning/MXnet/data'
+voc_dir = os.path.join(data_dir, 'VOCdevkit/VOC2012')
+
+num_workers = 0 if sys.platform.startswith('win32') else 4
+train_iter = gdata.DataLoader(
+    d2l.VOCSegDataset(True, crop_size, voc_dir, colormap2label), batch_size,
+    shuffle=True, last_batch='discard', num_workers=num_workers)
+test_iter = gdata.DataLoader(
+    d2l.VOCSegDataset(False, crop_size, voc_dir, colormap2label), batch_size,
+    last_batch='discard', num_workers=num_workers)
+
+# Train models
+ctx = d2l.try_all_gpus()
+loss = gloss.SoftmaxCrossEntropyLoss(axis=1)
+net.collect_params().reset_ctx(ctx)
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.1,
+                                                      'wd': 1e-3})
+d2l.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=5)
+
+#Prediction
+def predict(img):
+    X = test_iter._dataset.normalize_image(img)
+    X = X.transpose((2, 0, 1)).expand_dims(axis=0)
+    pred = nd.argmax(net(X.as_in_context(ctx[0])), axis=1)
+    return pred.reshape((pred.shape[1], pred.shape[2]))
+
+def label2image(pred):
+    colormap = nd.array(d2l.VOC_COLORMAP, ctx=ctx[0], dtype='uint8')
+    X = pred.astype('int32')
+    return colormap[X, :]
+
+test_images, test_labels = d2l.read_voc_images(is_train=False)
+n, imgs = 4, []
+for i in range(n):
+    crop_rect = (0, 0, 480, 320)
+    X = image.fixed_crop(test_images[i], *crop_rect)
+    pred = label2image(predict(X))
+    imgs += [X, pred, image.fixed_crop(test_labels[i], *crop_rect)]
+d2l.show_images(imgs[::3] + imgs[1::3] + imgs[2::3], 3, n);
+
